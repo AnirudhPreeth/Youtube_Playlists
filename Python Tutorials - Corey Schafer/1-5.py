87481ed9d222c4eb3b4322b55132c7e57f639314
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-13 21:54:58 +0530

    Update.

---
+++ Python Tutorials - Corey Schafer/1-5.py
@@ -0,0 +1,9 @@
+#Python Tutorials - Corey Schafer. 
+#https://www.youtube.com/playlist?list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU
+
+#This is the notes for the entire playlist and code as well. 
+
+#This file contains the notes and the code for videos 1-5. 
+
+'''
+'''
