4694d5ba3b54a50082f697852fe6dbf97a7a7d47
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-15 21:17:27 +0530

    Update.

--- Python Tutorials - Corey Schafer/6-10.py
+++ Python Tutorials - Corey Schafer/6-10.py
@@ -321,3 +321,25 @@ student_info('Math', 'Art', name = 'John', age = 22)
 #kwargs - dictionary with all of our keyword values. 
 #Function call with arguments using a * or **. When its used in that context, it will actually unpack a sequence or a dictionary and...
 #...pass those values into the function individually.  
+
+def student_info(*args, **kwargs):
+    print(args)  
+    print(kwargs)
+courses = ['Math', 'Art']
+info = {'name': 'John', 'age': 22}
+student_info('Math', 'Art', name = 'John', age = 22) 
+student_info(*courses, **info)
+
+#Number of days per month. FIrst value placeholder for indexing purposes.
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+def is_leap(year):
+    '''Return true for leap years, false for non-leap years.'''
+    return year%4 ==0 and (year%100 !=0 or year%400==0)
+def days_in_month(year, month):
+    '''Return number of days in that month in that year.'''
+    if not 1<=month <=12:
+        return 'Invalid month'
+    if month ==2 and is_leap(year):
+        return 29
+    return month_days[month]
+
---
+++ Vanilla JavaScript - Traversy Media/JavaScript Crash Course For Beginners/1 - JavaScript Crash Course For Beginners/1 - JavaScript Crash Course For Beginners.js
@@ -0,0 +1,21 @@
+//JavaScript Crash Course For Beginners - Traversy Media. 
+
+//https://youtu.be/hdI2bqOjy3c
+
+/*
+What is Javascript -
+Hight Level, interpreted programming language. 
+Conforms to the ECMAScript specification. 
+Multi-paradigm. Runs on client/browser as well as on the server (Node.js).
+Java is a complied language, Javascript is a scripting language. 
+Javascript is the language for the frontend. Javascript run time (Node.js). 
+Language of the browser. 
+Build very interactive user interfaces with frameworks like React.
+Used in building very fast server side and full stack applications.
+Used in mobile development (React Native, NativeScript, Ionic).
+Used in desktop application development (Electron JS).
+*/
+
+
+
+
