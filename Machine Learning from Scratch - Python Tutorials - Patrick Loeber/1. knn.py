a866964dbc77813c8e6c0a7a2f1c272fcb917de8
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-16 23:48:44 +0530

    Update.

---
+++ Machine Learning from Scratch - Python Tutorials - Patrick Loeber/1. knn.py
@@ -0,0 +1,11 @@
+#Machine Learning from Scratch - Python Tutorials - Patrick Loeber.
+#https://www.youtube.com/playlist?list=PLqnslRFeH2Upcrywf-u2etjdxxkL8nl7E
+
+#KNN (K Nearest Neighbors) in Python.
+
+#This is the notes for the entire playlist and code as well. 
+
+#This file contains the notes and the code for videos 1.
+
+'''
+'''
