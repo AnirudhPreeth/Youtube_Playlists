ebbe7c5076f03e3931c1d8e977ca08d1103fa68a
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-20 01:12:16 +0530

    Update.

--- Computer Science - Artificial Intelligence - NPTEL - Prof. Deepak Khemani/1 - Introduction.py
+++ Computer Science - Artificial Intelligence - NPTEL - Prof. Deepak Khemani/1 - Introduction.py
@@ -47,6 +47,107 @@ Analytic Engine.
 '''
 AI - Introduction. Video 3. 
 
-The emergence of mind in philosophy. 
+The emergence of mind in philosophy. Notion of the mind. 
+Correspondence theory of truth. 
+Perception is an internal process - Galileo Galilei.
+Thinking is the manipulation of symbols - Thomas Hobbes. 
+Reasoning = Computation. 
+Thoughts = Symbols (Rene Descartes). 
+Dualism:Mind and Body. Descartes - A symbol and what it symbolizes are two different things. 
+Symbol -> Algebraic manipulation = thinking. 
+What it symbolizes -> Subject of thought. Mind != World. 
+What makes a notion suitable for symbolizing? What makes a suitable notion actually symbolize?
+How can thought and matter interact? -> Mind-body problem. 
+John Haugeland - AI the Very Idea. 
+Paradox of Mechanical Reason - IF Reasoning is the manipulation of meaningful symbols according to rational rules THEN...
+...Who is manipulating the symbols?
+It can be either mechanical or meaningful but how can it be both? How can a mechanical manipulator pay attention to meaning?
+Humunculus. 
+Douglas Hofstadter. 
+John Locke - Father of Classical Liberalism. Mind was a blank state or tabula rasa. 
+David Hume. 
+Immanuel Kant. Kant vs Mills philosophy. 
+Ontology, Epistemology. 
+The bounds of our own mind. Epistemology of Transcendental Idealism. Autonomy of practical reason. 
+Kant claimed to have created "Copernican Revolution" in philosophy.
+Synthesis.  
+'''
+# That's it for that video. 
 
 '''
+AI - Introduction. Video 4. 
+
+How AI got it's name - John McCarthy and Marvin Minsky. 
+Lisp language. 
+Minsky - frames (Object Oriented Programming). 
+Nathaniel Rochester - Samuel Program writing checkers. 
+Herbert Simon and Alan Newell. J.C.Shaw. 
+Logic Theorist (LT) program. 
+Symbol, Symbol System, Physical System Symbols.
+Physical Symbol System Hypothesis - Physical Symbol System has the necessary and sufficient means for general intelligent action. 
+Ability to manipulate symbols - Symbolic AI/ Classical AI. Good old Fashioned Artificial Intelligence (GOFAI). 
+Three Laws of Robotics - Issac Asimov. ("Runaround").
+Cray Blitz wins Mississippi State Championship - First Computer to beat a master in tournament play.
+HiTech - beats Arnold Denker. 
+
+AI Landmarks -
+1962 - Unimation - First Industrial Robots. Jaakko Hintikka writes "Knowledge and Belief". 
+       Saul Kripke - Kripke Structures for possible world semantics. 
+1963 - Ivan Sutherland (PhD dissertation) Sketchpad: CAD program.
+       Ross Quillian: Semantic Nets.
+       Susomo Kuno's parser at MIT tested on "Time flies like an arrow".
+       Edward Feigenbaum and Julian Feldman publish "Computers and thought".
+1964 - Daniel Borbrow (PhD dissertation) STUDENT - solves algebra problems
+       Bertram Raphael (PhD dissertation) SIR - on knowledge representation for question answering. 
+1965 - Alan Robinson - Resolution Method for theorem proving. 
+       Ivan Sutherland and Bob Sprouli - Virtual Reality with head mounted display. 
+1968 - Joseph Moses (PhD dissertation) MACSYMA - symbolic reasoning in mathematics.
+1969 - SHAKEY - robot demonstrated at Stanford Research Institute.
+1970 - Bill Woods: Transition Networks for Natural language Parsing. 
+1971 - Alain Colmerauer develops Prolog. 
+       Earl Sacerdoti: Hireachical planning with ABSTRIPS. 
+       Nils Nilsson and Richard Fikes demonstrate planning system STRIPS. 
+       Terry Winograd (PhD dissertation) SHRDLU understanding English in restricted domain. 
+1977 - SRI's PROSPECTOR expert system predicts existence of hithero unknown molybdenum deposit.
+1978 - Stefik and Friedland's MOLGEN demonstrates utility of Object Oriented Programming. 
+1980 - First AAAI Conference. McDermott builds XCON expert system.
+1985 - AARON - drawing artist created by Harlod Cohen demonstrated at AAAI.
+1989 - Dean Pomerleau at CMU created ALVINN (An Autonomous Land Vehicle in a Neural Network).
+2004 - OWL Web Ontology Language W3C Recommendation.
+       DARPA introduced Grand Challenge for autonomous vehicles for prize money.
+2005 - Honda ASIMO robot. 
+       Blue Brain is born. 
+2006 - Darthmouth AI Conference (Next 50 years).
+2013 - Japanese space agency launches Kirobo into space as companion for human. 
+
+SHAKEY - Autonomous Robot. 
+Haugeland - The goal of AI is to build a machine with a mind of its own. 
+'''
+# That's it for that video. 
+
+'''
+AI - Introduction. Video 5. 
+
+Reality. Objective reality. 
+Complexity. 
+The World in our Minds. 
+Symbol - perceptible. 
+"Powers of 10" - Charles and Ray Eames (1977) - Film.
+Epiphenomenon in computers. 
+Douglas Hofstader - I am a Strange Loop.
+Rusell and Whitehead Principia Mathematica.
+Intelligent Agents - Persistent, Autonomous, Proactive, Goal Directed. 
+Signal -> Symbol -> Signal. 
+Character recognition. 
+Sense -> Deliberate -> Act. 
+
+Speech recognition, Image processing, Computer Vision, Neural Networks, Qualitative Reasoning, Natural Language Understanding...
+...Natural Language Generation, Speech Synthesis, Knowledge representation, logic, graphics, ontology, semantics, models, search...
+...pattern recognition, knowledge discovery, memory, planning, adversarial reasoning, handling reasoning, handling uncertainty...
+...tactile sensing, robot control, speech synthesis etc. 
+
+Artificial Intelligence, Planning and constraint satisfaction, Machine Learning, Memory Based Reasoning in AI.
+Knowledge Representation and Reasoning, Speech Technology, Computer Vision. 
+'''
+
+# That's it for that video. 
