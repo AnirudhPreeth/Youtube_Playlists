a89246c1104a90cc0e46a09a7016c35055640497
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-19 19:54:03 +0530

    Update.

---
+++ Computer Science - Artificial Intelligence - NPTEL - Prof. Deepak Khemani/1 - Introduction.py
@@ -0,0 +1,52 @@
+#Computer Science - Artificial Intelligence - NPTEl - Prof. Deepak Khemani 
+#https://www.youtube.com/playlist?list=PLbMVogVj5nJQu5qwm-HmJgjmeGhsErvXD
+
+#These are the notes I have jotted down during the course.
+
+'''
+AI - Introduction. Video 1.
+
+David Levy - Chess dude. 
+Herbert Simon, Avron Barr, Edward Feigenbaum, Elaine Rice, Eugene Charniak. 
+Hard problem in polynomial-time. You cannot solve hard problems in polynomial-time.
+Ai approach - cognitive approach or engineering approach. 
+John Haugeland. Thomas Hobbes.
+Inductive inferences, ability to generalize.
+Use of language. 
+Emotion Machine - Mavin Minsky. 
+Herbert Dreyfus, John Searle, Roger Penrose. 
+John Searle - Chinese Room Argument. 
+How many rules will the agent need to have for the thought experiment to be convincing?
+The Emperor's New Mind - Roger Penrose.
+Alan Turing's Imitation Game - Turing Test. 
+Loebner Prize. 
+Eliza Program - Name after Eliza Doolittle, written by Joseph Weizenbaum in 1966. 
+'''
+# That's it for that video. 
+
+'''
+AI - Introduction. Video 2.
+
+Kirobo (Robot). 
+David Levy - 2008 - Robot companions. 
+Love and Sex with Robots - David Levy. 
+Language or thought? What came first?
+Noam Chomsky - Linguistics - Universal Grammer. 
+Pamela McCorduck - Machines Who Think. 
+Vaucanson's Duck - Jacques de Vaucanson. 
+Von Kempelen's Chess Playing Turk - Wolfgang von Kempelen. 
+Pascal's calculator. 
+Leibniz Wheel. The Stepped Reckoner. 
+Leibniz - Calculus Ratiocinator. 
+Arithmometer - Thomas de Colmar.
+The Difference Engine. Jaquard Looms. 
+Analytic Engine. 
+'''
+# That's it for that video. 
+
+'''
+AI - Introduction. Video 3. 
+
+The emergence of mind in philosophy. 
+
+'''
